0b0b6931b635e8e7cc4a7cea7cbb7b21c2808b2b
Usama Hussain <uhussain@wisc.edu>
2019-03-06 16:40:37 +0100

    Recipe for Fall17Samples and for Moriond18 synced with HZZ

--- recipe/setup.sh
+++ recipe/setup.sh
@@ -40,27 +40,32 @@ fi
 
 pushd $CMSSW_BASE/src
 
+#Follow: https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsZZ4l2018
+#Global Tag
+#Data: 94X_dataRun2_v6
+#MC: 94X_mc2017_realistic_v13
+
 #ECAL scale and resolution corrections for Moriond18: Run2017_17Nov2017_v1 (time-eta-r9 categories)
 #Release is based on CMSSW_9_4_0. Scale and resolution corrections are based on the Golden JSON file of 2017 data (17Nov re-reco)
 # /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt
 
-# if [ ! -d ./EgammaAnalysis ]; then
-#  echo "Setting up electron energy scale corrections"
-#  #add the repository with the updated Egamma package
-#  git cms-merge-topic cms-egamma:EGM_94X_v1
-#  pushd EgammaAnalysis/ElectronTools/data
-#  # download the txt files with the corrections
-#  git clone https://github.com/ECALELFS/ScalesSmearings.git
-#  pushd ScalesSmearings/
-#  git checkout Run2017_17Nov2017_v1
-#  popd
-#fi
-git cms-merge-topic cms-egamma:EgammaID_949 #if you want the V2 IDs, otherwise skip
+if [ ! -d ./EgammaAnalysis ]; then
+  echo "Setting up electron energy scale corrections"
+  #add the repository with the updated Egamma package
+  git cms-merge-topic cms-egamma:EGM_94X_v1
+  pushd EgammaAnalysis/ElectronTools/data
+  # download the txt files with the corrections
+  git clone https://github.com/ECALELFS/ScalesSmearings.git
+  pushd ScalesSmearings/
+  git checkout Run2017_17Nov2017_v1
+  popd
+fi
 
-git cms-merge-topic cms-egamma:EgammaPostRecoTools_940 #just adds in an extra file to have a setup function to make things easier
-
-# Get recipes to re-correct MET (also for ECAL noise)
-git cms-merge-topic cms-met:METFixEE2017_949_v2
+#ElectronMVA ID in 94X according to https://twiki.cern.ch/twiki/bin/viewauth/CMS/MultivariateElectronIdentificationRun2#Recipes_for_regular_users
+# MVA ID V2 now, not yet available as part of official recepie
+git cms-merge-topic guitargeek:ElectronID_MVA2017_V2_HZZ_940pre3
+rm -rf RecoEgamma/ElectronIdentification/data #Delete old BDT weights so we can clone new ones
+git clone -b ElectronID_MVA2017_V2 https://github.com/guitargeek/RecoEgamma-ElectronIdentification RecoEgamma/ElectronIdentification/data/
 
 #in the EgammaAnalysis/ElectronTools/python/calibrationTablesRun2.py file the you have to make sure that the correctionType 
 #is the appropriate for your dataset. For the Run2017_17Nov2017_v1 it has to be:
@@ -68,17 +73,7 @@ git cms-merge-topic cms-met:METFixEE2017_949_v2
 # The corrections contained in the repository with tag "Run2017_17Nov2017_v1" include updated numbers for the corrections and 
 #preliminary conservative version of the systematics.
 
-##########################For Moriond17
-# if [ ! -d ./EgammaAnalysis ]; then
-#     echo "Setting up electron energy scale corrections"
-#     git cms-merge-topic rafaellopesdesa:EgammaAnalysis80_EGMSmearer_Moriond17_23Jan
-#
-#     pushd EgammaAnalysis/ElectronTools/data
-#     git clone https://github.com/ECALELFS/ScalesSmearings.git
-#     popd
-# fi
-##############################
-
+#####################################NOT USING THIS AT THE MOMENT
 if [ "$HZZ" ]; then
 
     if [ ! -d ./ZZMatrixElement ]; then
@@ -121,12 +116,24 @@ if [ "$HZZ" ]; then
     cp UWVV/AnalysisTools/plugins/ZZDiscriminantEmbedderCode.txt UWVV/AnalysisTools/plugins/ZZDiscriminantEmbedder.cc
     cp UWVV/AnalysisTools/plugins/ZKinematicFitEmbedderCode.txt UWVV/AnalysisTools/plugins/ZKinematicFitEmbedder.cc
 fi
+#####################################NOT USING THIS AT THE MOMENT
 
-#if [ $MET -ne 0 ] && [ ! -d ./RecoMET ]; then
-#    echo -e "\nChecking out MET recipe for Moriond 17"
-#    git cms-merge-topic -u cms-met:METRecipe_8020
-#fi
-
+##IF intetested in re-clustering MET
+#https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription#Instructions_for_9_4_X_X_0_for_M
+#metTag = cms.InputTag("slimmedMETs")
+#from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+#process.testCands = cms.EDFilter("CandPtrSelector",
+#                     src=cms.InputTag("packedPFCandidates"),
+#                     cut=cms.string("abs(eta)<2.5")
+#                      )
+#runMetCorAndUncFromMiniAOD(process,
+#                           isData=(not IsMC),
+#                           pfCandColl=cms.InputTag("testCands"),
+#                           reclusterJets=True,
+#                           recoMetFromPFCs=True,
+#                           postfix="Test",)
+#metTag = cms.InputTag("slimmedMETsTest","","ZZ")
+#process.MET = cms.Path(process.testCands + process.fullPatMetSequenceTest)
 
 if [ ! -d ./KaMuCa ]; then
     echo "Setting up muon calibration"
