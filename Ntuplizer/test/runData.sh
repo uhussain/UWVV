c4165dd9de46a8e58d61ea59a36410a6b270fa87
Usama Hussain <uhussain@wisc.edu>
2019-03-08 22:20:32 +0100

    Run stuff

---
+++ Ntuplizer/test/runData.sh
@@ -0,0 +1,2 @@
+#cmsRun ntuplize_cfg.py channels=zz isMC=1 eCalib=1 muCalib=1 globalTag=102X_dataRun2_Prompt_v11
+cmsRun ntuplize_cfg.py channels=zz isMC=1 eCalib=1 muCalib=1 globalTag=102X_dataRun2_Sep2018Rereco_v1  
