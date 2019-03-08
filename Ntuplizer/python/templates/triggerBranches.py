78db0142d78c884a8e48ed9791da723bacb9f5c6
Usama Hussain <uhussain@wisc.edu>
2019-03-08 15:32:09 +0100

    Latest 2018 triggers

--- Ntuplizer/python/templates/triggerBranches.py
+++ Ntuplizer/python/templates/triggerBranches.py
@@ -47,7 +47,7 @@ zzCompositeTriggerBranches = cms.PSet(
 verboseTriggerBranches = cms.PSet(
     trigNames = cms.vstring(
          "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8",#2018/2017
-         "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8",
+         "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8",#2017
          "HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL",#2018/2017
          "HLT_DoubleEle25_CaloIdL_MW",#2018
          "HLT_DoubleEle33_CaloIdL_MW",#2017
@@ -55,9 +55,9 @@ verboseTriggerBranches = cms.PSet(
          "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",#2018/2017
          "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",#2018/2017
          'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ',#2018/2017
-         "HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL",
-         "HLT_Mu8_DiEle12_CaloIdL_TrackIdL",
-         "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ",
+         "HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL",#2017
+         "HLT_Mu8_DiEle12_CaloIdL_TrackIdL",#2017
+         "HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ",#2017/2018
          "HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ",#2018/2017
          "HLT_TripleMu_12_10_5",#2018/2017
          "HLT_TripleMu_10_5_5_DZ",#2018/2017
@@ -71,7 +71,7 @@ verboseTriggerBranches = cms.PSet(
     #HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVLPaths = cms.vstring('HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v[0-9]+'),
     #HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVLPaths = cms.vstring('HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v[0-9]+'),
     HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8Paths = cms.vstring('HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v[0-9]+'),#2018/2017
-    HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8Paths = cms.vstring('HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v[0-9]+'),
+    HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8Paths = cms.vstring('HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v[0-9]+'),#2017
     HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVLPaths = cms.vstring('HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_v[0-9]+'),#2018/2017
     #HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZPaths = cms.vstring('HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v[0-9]+'),
     HLT_DoubleEle25_CaloIdL_MWPaths = cms.vstring('HLT_DoubleEle25_CaloIdL_MW_v[0-9]+'),#2018
@@ -84,9 +84,9 @@ verboseTriggerBranches = cms.PSet(
     #HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVLPaths = cms.vstring('HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v[0-9]+'),
     #HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVLPaths = cms.vstring('HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v[0-9]+'),
     #HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZPaths = cms.vstring('HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v[0-9]+'),
-    HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdLPaths = cms.vstring('HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL_v[0-9]+'),
-    HLT_Mu8_DiEle12_CaloIdL_TrackIdLPaths = cms.vstring('HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v[0-9]+'),
-    HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZPaths = cms.vstring('HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ_v[0-9]+'),
+    HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdLPaths = cms.vstring('HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL_v[0-9]+'),#2017
+    HLT_Mu8_DiEle12_CaloIdL_TrackIdLPaths = cms.vstring('HLT_Mu8_DiEle12_CaloIdL_TrackIdL_v[0-9]+'),#2017
+    HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZPaths = cms.vstring('HLT_Mu8_DiEle12_CaloIdL_TrackIdL_DZ_v[0-9]+'),#2017/2018
     HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZPaths = cms.vstring('HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ_v[0-9]+'),#2018/2017
     HLT_TripleMu_12_10_5Paths = cms.vstring('HLT_TripleMu_12_10_5_v[0-9]+'),#2018/2017
     HLT_TripleMu_10_5_5_DZPaths = cms.vstring('HLT_TripleMu_10_5_5_DZ_v[0-9]+'),#2018/2017
